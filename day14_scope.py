d4bd76cecb9a6060f54c058a02c018d2f7cfa795
shakoortaj <shakoortaj@gmail.com>
2019-10-22 22:50:49 +0500

    day14_scope challenge solved HR

---
+++ day14_scope.py
@@ -0,0 +1,18 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    # Add your code here
+    def computeDifference(self):
+        first_element = self.__elements[0]  # b[0]
+        next_elements = self.__elements[1::]  # b[1::]
+        max_diff = [abs(first_element - element) for element in next_elements]
+        maximumDifference = max(max_diff)
+        return maximumDifference
+
+a = [1,2,5,7]
+
+d = Difference(a)
+d.computeDifference()
+m = d.computeDifference
+print(d.computeDifference())
